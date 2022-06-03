c345227752727fda033321a13cc37b59487c3c30
Limezy <arnaudetpia@de-ruffray.fr>
2022-06-04 02:03:27 +0700

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 NODEJS_VERSION=14
 GO_VERSION="1.17"
 
-pkg_dependencies="npm libc6-dev libssl-dev libxft-dev libhdf5-serial-dev libpng-dev libheif-examples librsvg2-bin libx264-dev libx265-dev libnss3 libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev"
+pkg_dependencies="npm libc6-dev libssl-dev libxft-dev libhdf5-serial-dev libpng-dev libheif-examples librsvg2-bin libx264-dev libx265-dev libnss3 libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev libzmq3-dev"
 
 #=================================================
 # PERSONAL HELPERS
