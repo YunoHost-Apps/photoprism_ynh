4fd77c449ce2d6c62147e339ba5c0913610d6c97
yalh76 <yalh@yahoo.com>
2022-06-23 14:38:23 +0200

    First docker

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,13 +5,14 @@
 #=================================================
 
 # Tag name of the installed version
-PHOTOPRISM_VERSION="220617-0402b8d3"
+PHOTOPRISM_VERSION="220617-bullseye"
 
 MEMORY_NEEDED="3000"
 NODEJS_VERSION=14
 GO_VERSION="1.18"
 
-pkg_dependencies="libc6-dev libssl-dev libxft-dev libhdf5-serial-dev libpng-dev libheif-examples librsvg2-bin libx264-dev libx265-dev libnss3 libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev libzmq3-dev"
+#pkg_dependencies="libc6-dev libssl-dev libxft-dev libhdf5-serial-dev libpng-dev libheif-examples librsvg2-bin libx264-dev libx265-dev libnss3 libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev libzmq3-dev"
+pkg_dependencies=""
 
 #=================================================
 # PERSONAL HELPERS
