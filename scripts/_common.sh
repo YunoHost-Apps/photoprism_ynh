deb0219165c30f9e65492ebe1e2e9c321c705232
navanchauhan <navanchauhan@gmail.com>
2022-08-05 21:59:52 -0400

    specify binary path envs

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 PHOTOPRISM_VERSION="220730-bullseye"
 
 #pkg_dependencies="libc6-dev libssl-dev libxft-dev libhdf5-serial-dev libpng-dev libheif-examples librsvg2-bin libx264-dev libx265-dev libnss3 libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev libzmq3-dev"
-pkg_dependencies=""
+pkg_dependencies="ffmpeg libimage-exiftool-perl"
 
 #=================================================
 # PERSONAL HELPERS
