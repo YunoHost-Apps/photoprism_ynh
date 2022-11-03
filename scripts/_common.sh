fd0efde85f54dd4bb1d5832426ae76df63ee115c
Thomas <51749973+Thovi98@users.noreply.github.com>
2022-11-03 18:31:51 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,15 +7,15 @@
 function detect_arch() {
   case "$YNH_ARCH" in
     "amd64")
-      PHOTOPRISM_VERSION="sha256:3381d40181ecdf62932e9530c6b81e6e0f8828a3d373d8dedf662aab96dafba4"
+      PHOTOPRISM_VERSION="sha256:2e420a23ab8f3ce6bb5cb7719a93a59383a8361c566dea5b7f18ef466876b379"
       ;;
 
     "arm64")
-      PHOTOPRISM_VERSION="sha256:3dcffda0a2b58c91479c883205025edee4ea799fc35be419f9bec708ccb54b7f"
+      PHOTOPRISM_VERSION="sha256:64606bfc48660585d0bb60ebedf1ad4587a8aedd162291a5459f08a2e3f9173b"
       ;;
 
     "armhf")
-      PHOTOPRISM_VERSION="sha256:a74f4d0a220af9a5291307bfba36f662d97c4df143f04d951e3c30111916317e"
+      PHOTOPRISM_VERSION="sha256:899e794338e4f31b6768f971b0159d8762a71a204ee6c8c651ffdb79fdd5cfa1"
       ;;
 
     *)
